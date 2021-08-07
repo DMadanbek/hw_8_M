69d9329530e1255a32612c5ef01910975c2f52bc
DMadanbek <84191060+DMadanbek@users.noreply.github.com>
2021-08-07 23:41:52 +0600

    Add files via upload

---
+++ product/models.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name=models.CharField(max_length=100)
+    class Meta:
+        verbose_name="Тег"
+        verbose_name_plural="Теги"
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Заголовок")
+    description = models.TextField(null=True,blank=True,verbose_name="Описание")
+    price = models.FloatField(verbose_name="Цена")
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,verbose_name="Категория")
+    tags = models.ManyToManyField(Tag, blank=True,verbose_name="Теги")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.title
+
+class Reciew(models.Model):
+    text = models.TextField(verbose_name="Текст")
+    product = models.ForeignKey(Products,on_delete=models.CASCADE, verbose_name="Продукт")
+    class Meta:
+        verbose_name = "Обзор"
+        verbose_name_plural = "Обзоры"
+
+    def __str__(self):
+        return self.text
+
+class ConfirmCode(models.Model):
+    code=models.CharField(max_length=12)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    valid_until = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.code
+
+
+
---
+++ product/views.py
@@ -0,0 +1,155 @@
+import datetime
+
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from product.models import Category, ConfirmCode
+from product.models import Products
+from product.forms import ProductForm,RegisterForm
+from  django.contrib import auth
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+categories = Category.objects.all()
+
+def category_vievs(request,Category_id):
+    prod = Products.objects.filter(category_id=Category_id)
+    category = Category.objects.all()
+    produts = Products.objects.all()
+
+    data = {
+        "product_list": prod,
+        "Category_list": category,
+        "Product_list": produts
+    }
+    return render(request, "item.html", context=data)
+
+PAGE_SIZE=7
+def main_page_vievs(request):
+    page = int(request.GET.get("page", "1"))
+    print("Страница:", page)
+    print(f"Обьекты :[{(page - 1 * PAGE_SIZE)}:{page * PAGE_SIZE} ]")
+    products = Products.objects.all()
+    total = products.count()
+    page_count = total // PAGE_SIZE
+    if total % PAGE_SIZE > 0:
+        page_count = page_count + 1
+    next_page = page + 1
+    prev_page = page - 1
+    data = {
+        "title": "Главная страница",
+        "page": page,
+        "page_count": range(1, page_count + 1),
+        "next_page": next_page,
+        "prev_page": prev_page,
+        "last_page": page_count,
+        "product_list": products[(page - 1) * PAGE_SIZE:page * PAGE_SIZE],
+        "Category_list": categories,
+        "Product_list": products
+    }
+    return render(request, "index.html", context=data)
+
+def product_item_views(request,Product_id):
+    product = Products.objects.get(id=Product_id)
+    category = Category.objects.all()
+    produts = Products.objects.all()
+    data={
+        "product":product,
+        "Category_list": category,
+        "Product_list": produts
+    }
+    return render(request, "product.html", context=data)
+
+@login_required(login_url="/login/")
+def add_product(request):
+    if request.method =="GET":
+        data={
+            "form": ProductForm
+        }
+        return render(request,"add.html",context=data)
+    elif request.method=="POST":
+        form=ProductForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("/login/")
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+    data = {
+        "form": LoginForm()
+    }
+    return render(request, "Login.html", context=data)
+
+
+def register(request):
+    if request.method=="POST":
+        form=RegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/check/")
+        else:
+            data={
+                "form":form
+            }
+            return render(request,"register.html",context=data)
+    data = {
+        "form": RegisterForm()
+    }
+    return render(request,"register.html",context=data)
+
+
+def check(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+    data = {
+        "form": LoginForm()
+    }
+    return render(request, "check.html", context=data)
+def search(request):
+    query=request.GET.get("query","")
+    print(query)
+    products=Products.objects.filter(title__contains=query)
+    print(products.values())
+    return JsonResponse(data={"list":list(products.values())},safe=False)
+
+
+def javascript(request):
+    return render(request,"javascript.html")
+
+def javascript(request):
+    return render(request, 'javascript.html')
+
+def search(request):
+    query=request.GET.get("query","")
+    print(query)
+    products=Products.objects.filter(title__contains=query)
+    print(products.values())
+    return JsonResponse(data={"list":list(products.values())},safe=False)
+
+
+def activate(request, code):
+    print(code)
+    try:
+        user = ConfirmCode.objects.get(code=code,
+                                       valid_until__gte=datetime.datetime.now()).user
+        user.is_active = True
+        user.save()
+    except:
+        pass
+    return redirect('/login/')

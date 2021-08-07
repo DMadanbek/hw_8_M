011932c28b66e67df7cbf520eddf636930f38a75
DMadanbek <84191060+DMadanbek@users.noreply.github.com>
2021-08-07 23:40:51 +0600

    Add files via upload

---
+++ product/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from product.models import Category, ConfirmCode
+from product.models import Reciew
+from product.models import Products
+from product.models import Tag
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Reciew)
+admin.site.register(Products)
+admin.site.register(Tag)
+admin.site.register(ConfirmCode)
